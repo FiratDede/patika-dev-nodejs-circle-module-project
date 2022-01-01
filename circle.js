609bd5ed9c4c754c547c4537afe2cc5c9a7eb135
Fırat Dede <53142518+FiratDede@users.noreply.github.com>
2022-01-01 18:48:21 +0300

    Add files via upload

---
+++ circle.js
@@ -0,0 +1,7 @@
+function circleArea(radius){
+    return Math.PI*radius*radius;
+}
+function circleCircumference(radius){
+    return Math.PI*2*radius;
+}
+module.exports = {circleArea,circleCircumference};
---
+++ index.js
@@ -0,0 +1,4 @@
+let {circleArea,circleCircumference} = require("./circle");
+let myRadius= 5;
+console.log("Area of a circle which has 5 cm radius is "+circleArea(5)+" cm^2")
+console.log("Circumference of a circle which has 5 cm raidus is "+circleCircumference(5)+" cm")
